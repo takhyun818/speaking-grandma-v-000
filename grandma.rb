d0614d422199fa1928f9f4ddb8e604417fd8f561
Tak H Kim <takhyun818@gmail.com>
2018-09-12 21:39:30 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,8 +4,9 @@ def speak_to_grandma(phrase)
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-else
-  return "No, NOT SINCE 1938!"
+  else
+    return "No, NOT SINCE 1938!"
+  end
 end
 
 # Whatever you say to grandma, she should respond with
